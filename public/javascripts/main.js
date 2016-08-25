6afc0bced7253e24cbce6a5a76e6e7c8c7c9930a
Dewi Junita <dewi@gearev.com>
2016-08-25 10:46:23 +0700

    Update material design theme.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,17 @@
 $.material.init()
 
 $(function() {
-    $('#side-menu').metisMenu();
+    $(".button-collapse").sideNav();
 });
+
+$('.button-collapse').sideNav({
+     menuWidth: 300, // Default is 240
+     edge: 'right', // Choose the horizontal origin
+     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+   }
+ );
+
+// Show sideNav
+$('.button-collapse').sideNav('show');
+   // Hide sideNav
+$('.button-collapse').sideNav('hide');
