470c518715bb4939bda12f0978f0c39a09c1a484
Dewi Junita <dewi@gearev.com>
2016-09-07 17:16:05 +0700

    Update seeder with different place details.

--- seeders/20160825101431-create-default-places.js
+++ seeders/20160825101431-create-default-places.js
@@ -11,11 +11,11 @@ module.exports = {
             createdAt: '2016-08-26',
             updatedAt: '2016-08-26'
         }, {
-            name: 'John Doe',
-            address: 'BEC',
-            description: 'BEC',
-            longitude: 'BEC',
-            latitude: 'BEC',
+            name: 'Miata',
+            address: 'BIP',
+            description: 'Mall',
+            longitude: '3',
+            latitude: '4',
             createdAt: '2016-08-26',
             updatedAt: '2016-08-26'
         }], {});
