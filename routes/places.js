5c9be6263c2c4e77443518cb2881eefa98cac144
Dewi Junita <dewi@gearev.com>
2016-09-28 12:39:33 +0700

    fix issue #77: Update place link on side menu.

--- routes/places.js
+++ routes/places.js
@@ -4,13 +4,6 @@ var connection = require('../helper/connection');
 var mysql = require('mysql');
 
 /* GET home page. */
-/*router.get('/', function(req, res, next) {
-  res.render('place/index', {title : 'Place'});
-}); */
-router.get('/detail', function(req, res, next) {
-  res.render('place/detail', {title : 'Place Detail'});
-});
-
 
 /* GET places listing. */
 router.get('/', function index(req, res) {
@@ -28,10 +21,45 @@ router.get('/:id', function index(req, res) {
   sql = mysql.format(sql, inserts);
     connection.query(sql, function (err, rows) {
       if (err) throw err;
-      res.render('place/detail', {title : 'Detail Places',
+      res.render('place/detail', {title : 'Detail Place',
     place: rows[0]});
-    console.log(rows);
     });
 });
+
+router.get('/create', function create(req, res) {
+    console.log('tesssss');
+  var create = {
+    title: "Place - New Place",
+    navigationTitle: 'New Place',
+    action: '/create',
+    create: true,
+    data: {
+      name: 'name',
+      address: 'address',
+      description: 'description'
+    },
+  };
+
+  res.render('place/create', create);
+});
+
+router.post('/', function index(req, res) {
+  console.log('123');
+  delete req.body.id;
+  delete req.body.updatedAt;
+  delete req.body.createdAt;
+  var inserts = req.body;
+  inserts.createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  inserts.updatedAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  //inserts.createdBy = req.user.id;
+  connection.query('INSERT INTO places SET ?', inserts, function (err, result) {
+    if (err) throw err;
+    var contype = req.headers['content-type'];
+    if (!contype || contype.indexOf('application/json') !== 0)
+      res.redirect('/places/create');
+    else
+      res.send(result.insertId.toString());
+  });
+});;
 
 module.exports = router;
